91cb83af9675c5d061dce9511f00648559117f3c
Med <elmanioui.mehdi@gmail.com>
2022-02-13 13:46:59 +0100

    implemented sorting and filtering

--- task_manager.rb
+++ task_manager.rb
@@ -24,6 +24,38 @@ helpers do
   def size_of_incompleted_todos(list)
     list[:todos].reject {|todo| todo[:completed] }.size 
   end
+
+  # takes an array of hashes and reorders them while keeping record of their origianl index.
+  # it then yields each sorted hash with its original index to the explicit block
+  # this feature is intended exculsievly for the `lists` view template.
+   def sort_lists(lists, &block)
+    sorted_list = sorted(lists) {|list| list_complete?(list) ? 1 : 0 }
+    sorted_list.each(&block)
+  end
+
+  def sort_todos(list, &block)
+    sorted_list = sorted(list) { |todo| todo[:completed] ? 1 : 0 }
+    sorted_list.each(&block)
+  end
+
+
+  def sorted(list, &block)
+    list.map
+        .with_index {|item, index| [item, index] }
+        .sort_by {|item, index| yield(item) }
+  end
+
+=begin
+# Important Notes: Line 32 & Line 37
+  Ruby doesn't provide any built-in mechanism for comparing boolean values(`true` and `false`). 
+  I took advantage of the way `Enumerable#sort_by` works in order to sort boolean values based on integer values which do implement comparison(<=>) in their class.
+  `Enumerable#sort_by` works in 3 steps:
+      1. It yields each element of the caller collection to the block, and caches the return value of the block at each iteration temporarily.
+      2. sorts all of the block's cached returned values from step 1, which correspond to elements in the calling collection
+      3. returns a new sorted collection object(e.i Array), within which elements are sorted based on the sorted return values of th block from step # 2.
+  Note: This only works if the values returned by the block at each initial iteration(step #1) implement a comparison method and are of the same class(side_note: this could possibly be overidden with custom classes/objects)
+  example: suppose at step # 1, you invoke a custom method within the block. if the method returns `nil`,`fale`, `true` or raises an exception for any of the passed in elements, the method will not work.
+=end
 end
 
 before do
