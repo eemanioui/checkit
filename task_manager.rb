c0acd6b9687408ec1523a6c23049ddb464fcf66b
Med <elmanioui.mehdi@gmail.com>
2022-02-14 02:23:37 +0100

    added a NOT found route and reordered part of the code for better readability

--- task_manager.rb
+++ task_manager.rb
@@ -8,60 +8,7 @@ configure do
   set :session_secret, 'secret'
 end
 
-helpers do
-  def list_complete?(list)
-     size_of_todos(list) > 0 && size_of_incompleted_todos(list) == 0
-  end
-
-  def list_class(list)
-    "complete" if list_complete?(list)
-  end
-
-  def size_of_todos(list)
-    list[:todos].size
-  end
-
-  def size_of_incompleted_todos(list)
-    list[:todos].reject {|todo| todo[:completed] }.size 
-  end
-
-  # takes an array of hashes and reorders them while keeping record of their origianl index.
-  # it then yields each sorted hash with its original index to the explicit block
-  # this feature is intended exculsievly for the `lists` view template.
-   def sort_lists(lists, &block)
-    sorted_list = sorted(lists) {|list| list_complete?(list) ? 1 : 0 }
-    sorted_list.each(&block)
-  end
-
-  def sort_todos(list, &block)
-    sorted_list = sorted(list) { |todo| todo[:completed] ? 1 : 0 }
-    sorted_list.each(&block)
-  end
-
-# this method expects an implicit block to be passed as an argument when it is invoked.
-  def sorted(list)
-    list.map
-        .with_index {|item, index| [item, index] }
-        .sort_by {|item, index| yield(item) }
-  end
-
-=begin
-# Important Notes regarding Line 32 & Line 37
-  - Ruby doesn't provide any built-in mechanism for comparing boolean values(`true` and `false`). 
-  - I took advantage of the way `Enumerable#sort_by` works in order to sort boolean values based on integer values which do implement comparison(<=>) in their class.
-  - `Enumerable#sort_by` works in 3 steps:
-      1. It yields each element of the caller collection to the block, and caches the return value of the block at each iteration temporarily.
-      2. sorts all of the block's cached returned values from step 1, which correspond to elements in the calling collection
-      3. returns a new sorted collection object(e.i Array), within which elements are sorted based on the sorted return values of th block from step # 2.
-  
-# Note: 
-  - This only works if the values returned by the block at each initial iteration(step #1) implement a comparison method and are of the same class
-  - The above could possibly be overidden with custom classes/objects)
-# Example: 
-  - suppose at step # 1, you invoke a custom method within the block. if the method returns `nil`,`false`, `true` or raises an `exception`` for any of the passed in elements, `Enumrable#sort_by` will not work and an exception will be raised
-=end
-end
-
+# This will execute before any pattern is matched with a route.
 before do
   session[:lists] ||= []
 end
@@ -197,6 +144,10 @@ post "/lists/:list_id/complete_all" do |list_id|
   redirect "/lists/#{@list_id}"
 end
 
+not_found do
+  redirect "/lists"
+end
+
 # returns a String message if the user input is invalid otherwise returns nil if the name is valid.
 def invalid(user_input)
   unless user_input.size.between?(1, 100)
@@ -204,4 +155,58 @@ def invalid(user_input)
   end
 
   "Entry name must be unique." if session[:lists].any? { |list| yield list }
+end
+
+helpers do
+  def list_complete?(list)
+     size_of_todos(list) > 0 && size_of_incompleted_todos(list) == 0
+  end
+
+  def list_class(list)
+    "complete" if list_complete?(list)
+  end
+
+  def size_of_todos(list)
+    list[:todos].size
+  end
+
+  def size_of_incompleted_todos(list)
+    list[:todos].reject {|todo| todo[:completed] }.size 
+  end
+
+  # takes an array of hashes and reorders them while keeping record of their origianl index.
+  # it then yields each sorted hash with its original index to the explicit block
+  # this feature is intended exculsievly for the `lists` view template.
+   def sort_lists(lists, &block)
+    sorted_list = sorted(lists) {|list| list_complete?(list) ? 1 : 0 }
+    sorted_list.each(&block)
+  end
+
+  def sort_todos(list, &block)
+    sorted_list = sorted(list) { |todo| todo[:completed] ? 1 : 0 }
+    sorted_list.each(&block)
+  end
+
+# this method expects an implicit block to be passed as an argument when it is invoked.
+  def sorted(list)
+    list.map
+        .with_index {|item, index| [item, index] }
+        .sort_by {|item, index| yield(item) }
+  end
+
+=begin
+# Important Notes regarding Line 176 & Line 181
+  - Ruby doesn't provide any built-in mechanism for comparing boolean values(`true` and `false`). 
+  - I took advantage of the way `Enumerable#sort_by` works in order to sort boolean values based on integer values which do implement comparison(<=>) in their class.
+  - `Enumerable#sort_by` works in 3 steps:
+      1. It yields each element of the caller collection to the block, and caches the return value of the block at each iteration temporarily.
+      2. sorts all of the block's cached returned values from step 1, which correspond to elements in the calling collection
+      3. returns a new sorted collection object(e.i Array), within which elements are sorted based on the sorted return values of th block from step # 2.
+
+  - The above works if the values returned by the block at each initial iteration(step #1) implement a comparison method and are of the same class
+  - The above could possibly be overidden with custom classes/objects)
+# Example: 
+  - suppose at step # 1, you invoke a custom method within the block. if the method returns `nil`,`false`, `true` or raises an `exception`` for any of the passed in elements, 
+   `Enumrable#sort_by` will not work and an exception will be raised.
+=end
 end
